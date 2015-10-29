22c52339ef32de3797a03275786a98e476de5c9c
Prashant-Surya <prashantsurya@ymail.com>
2015-10-29 19:13:34 +0530

    Fully working backend

--- codetable/views.py
+++ codetable/views.py
@@ -9,35 +9,33 @@ import requests
 def home(request):
     #form = CodeForm()
     if request.method=="POST":
-        form = CodeForm(request.POST)
+        print("invalid")
+        form = CodeForm(data = request.POST)
+        print(form.errors)
         if form.is_valid():
+            print("Valid")
             data = form.data['text']
             RUN_URL = u'https://api.hackerearth.com/v3/code/run/'
             secret = "2a14a05d9c4e68f793ed8749117cb63b28dcb016"
             stdin = form.data['inp']
-            #lang = form.data['langs']
-            ll = form.cleaned_data['langs']
-            lang = dict(form.fields['langs'].choices)[ll]
-            #obj.lang = lang
+            lang = form.cleaned_data['langs']
+            #lang = dict(form.fields['langs'].choices)[ll]
             source = data
             json_src = {
                'client_secret': secret,
                 'async': 0,
                 'source': source,
                 'lang': lang,
-                'input':"Checking Input",
-                'time_limit': 5,
-                'memory_limit': 262144,
+                'input':stdin,
             }
-            # r = requests.post(RUN_URL,data=json_src)
-            # r1 = r.json()
+            r = requests.post(RUN_URL,data=json_src)
+            r1 = r.json() 
             obj = Code()
             obj.code = data
             obj.lang = lang
-            # output = str(r1['run_status']['output'])
-            # obj.hash = str(r1['code_id'])
-            obj.hash="test2"
-            hash = "test2"
+            output = str(r1['run_status']['output'])
+            obj.hash = str(r1['code_id'])
+            hash = str(r1['code_id'])
             obj.publish()
             output="debug"
             form2 = CodeForm()
@@ -52,16 +50,37 @@ def display(request,hash):
     source = obj.code
     lang = obj.lang
     ip = request.session['i']
-    output = "no process"
+    output = ""
     form = CodeForm(initial = {'text':source,'inp':ip,'langs':lang})
+    RUN_URL = u'https://api.hackerearth.com/v3/code/run/'
+    secret = "2a14a05d9c4e68f793ed8749117cb63b28dcb016"
     if request.method == "POST":
+        form = CodeForm(data = request.POST)
         if form.is_valid():
-            form = CodeForm(request.POST)
             stdin = form.data['inp']
             source = form.data['text']
-            #lang = form.data['lang']
-            ll = form.cleaned_data['langs']
-            lang = dict(form.fields['langs'].choices)[ll]
-            output = "processed"
-            form = CodeForm(initial = {'text':source,'inp':stdin,'langs':lang})
+            lang = form.cleaned_data['langs']
+            #lang = dict(form.fields['langs'].choices)[ll]
+            
+            json_src = {
+               'client_secret': secret,
+                'async': 0,
+                'source': source,
+                'lang': lang,
+                'input':stdin,
+            }            
+            r = requests.post(RUN_URL,data=json_src)
+            r1 = r.json()
+            #output = r1
+            output = str(r1['run_status']['output'])
+            obj.code = source
+            obj.lang = lang
+            obj.publish()
+
+            dict_data = {}
+            dict_data['text'] = source
+            dict_data['inp'] = stdin
+            dict_data['langs'] = lang
+            form = CodeForm(initial = dict_data)
+
     return render(request,'codetable/display.html',{'form':form,'out':output})
