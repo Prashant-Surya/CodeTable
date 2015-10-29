cc00a43f610a855f02e57295078d38cd89e21f40
Prashant-Surya <prashantsurya@ymail.com>
2015-10-29 19:21:34 +0530

    rechecked input field and still need to change to display errors

--- codetable/forms.py
+++ codetable/forms.py
@@ -3,7 +3,7 @@ from .models import Code
 
 class CodeForm(forms.Form):
 	text = forms.CharField(widget = forms.Textarea(attrs={'id':'code'}))
-	inp = forms.CharField(widget = forms.Textarea(attrs={'id':'input'}))
+	inp = forms.CharField(widget = forms.Textarea(attrs={'id':'input'}),required=False)
 	l = [('C', 'C'), ('CPP', 'C++'), ('CPP11', 'C++11'), ('CLOJURE', 'Clojure'), ('CSS', 'CSS'), ('CSHARP', 'C#'), 
 	('HTML', 'HTML'), ('GO', 'Go'), ('JAVA', 'Java'), ('JAVASCRIPT', 'Javascript(Rhino)'),
 	 ('JAVASCRIPT_NODE', 'Javascript(Node.js)'), ('HASKELL', 'Haskell'), ('PERL', 'Perl'), 
