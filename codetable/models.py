543da6b7979f948e922b3e8da11d36794126add2
Prashant-Surya <prashantsurya@ymail.com>
2015-11-02 20:11:49 +0530

    Small changes

--- codetable/models.py
+++ codetable/models.py
@@ -3,10 +3,7 @@ from django.utils import timezone
 from django import forms
 
 class Code(models.Model):
-	#code = models.CharField(widget=forms.Textarea)
-	code = models.CharField(max_length=20000)
-	#code = forms.Textarea()
-	#code = 
+	code = models.CharField(max_length=20000) 
 	lang = models.CharField(max_length=20,default="C")
 	hash = models.CharField(max_length=20)
 	url_count = models.IntegerField(default=0)
